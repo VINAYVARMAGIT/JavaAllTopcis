2b508aade1d2666afe13c6783df22a9a2b26287b
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-09-09 21:57:04 +0530

    Customized ArrayList

---
+++ CollectionFrameworks/customArrayList.java
@@ -0,0 +1,43 @@
+package CollectionFrameworks;
+
+import java.util.Arrays;
+
+public class customArrayList<T> {
+
+    private Object[] array ;
+    private int size=0;
+    private int capaticyOfArray=10;
+
+    public customArrayList(){
+        array= new Object[capaticyOfArray];
+    }
+    public void add(T element){
+        if (size==array.length) {
+            resize();
+        }
+        array[size++]= element;
+    }
+
+    public T get(int index){
+        if (index<0 || index>= array.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return (T) array[index];
+    }
+    public int size(){
+        return size;
+    }
+    public void remove(int index){
+        if (index<0 || index>= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for (int i = index; i < size; i++) {
+            array[i]= array[i+1];
+        }
+        array[--size]=null;
+    }
+    public void resize(){
+        int newCapacity = array.length*2;
+        array = Arrays.copyOf(array, newCapacity);
+    }
+}
