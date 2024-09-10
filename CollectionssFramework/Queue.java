cc338e2f3dc8e457abb914cffa7f052eec4f42e0
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-09-10 17:49:23 +0530

    Queue implementation

--- CollectionssFramework/Queue.java
+++ CollectionssFramework/Queue.java
@@ -1,5 +1,43 @@
 package CollectionssFramework;
 
-public class Queue {
+import javax.management.RuntimeErrorException;
 
+public class Queue {
+   private int[] queue;
+   private int front;
+   private int rear;
+   private int size;
+   private int capacity;
+
+   public Queue(int capacity){
+    this.capacity=capacity;
+    queue = new int[capacity];
+    front =0;
+    rear=-1;
+    size=0;
+   }
+
+   public void enqueue(int element){
+    if (isFull()) {
+        throw new RuntimeException("Queue is Full");
+    }
+     rear = (rear+1)%capacity;
+     queue[rear]=element;
+     size++;
+   }
+   public boolean isFull(){
+    return size==capacity;
+   }
+   public int dequeue(int element){
+    if (isEmpty()) {
+        throw new RuntimeException("Queue is Empty");
+    }
+    int removedElement =queue[front];
+     front =(front+1)%capacity;
+     size--;
+     return removedElement;
+   }
+   public boolean isEmpty(){
+    return size==0;
+   }
 }
