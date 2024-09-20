ed31814a7544775738101b69d15c4e6b1c0fe7c4
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-09-20 09:56:09 +0530

    Queue implementation modified

--- CollectionssFramework/Queue.java
+++ CollectionssFramework/Queue.java
@@ -1,41 +1,85 @@
 package collectionssframework;
 
-public class Queue {
-   private int[] queue;
-   private int front;
-   private int rear;
-   private int size;
-   private int capacity;
+public class Queue<T> {
+  private Node<T> front;
+  private Node<T> rear;
+  private int size;
 
-   public Queue(int capacity){
-    this.capacity=capacity;
-    queue = new int[capacity];
-    front =0;
-    rear=-1;
-    size=0;
-   }
+  private static class Node<T> {
+      T data;
+      Node<T> next;
 
-   public void enqueue(int element){
-    if (isFull()) {
-        throw new RuntimeException("Queue is Full");
-    }
-     rear = (rear+1)%capacity;
-     queue[rear]=element;
-     size++;
-   }
-   public boolean isFull(){
-    return size==capacity;
-   }
-   public int dequeue(int element){
-    if (isEmpty()) {
-        throw new RuntimeException("Queue is Empty");
-    }
-    int removedElement =queue[front];
-     front =(front+1)%capacity;
-     size--;
-     return removedElement;
-   }
-   public boolean isEmpty(){
-    return size==0;
-   }
+      Node(T data) {
+          this.data = data;
+          this.next = null;
+      }
+  }
+
+  public Queue() {
+      front = null;
+      rear = null;
+      size = 0;
+  }
+
+  // Add an element to the rear of the queue
+  public void enqueue(T item) {
+      Node<T> newNode = new Node<>(item);
+      if (isEmpty()) {
+          front = newNode;
+          rear = newNode;
+      } else {
+          rear.next = newNode;
+          rear = newNode;
+      }
+      size++;
+  }
+
+  // Remove and return the element at the front of the queue
+  public T dequeue() {
+      if (isEmpty()) {
+          throw new IllegalStateException("Queue is empty");
+      }
+      T item = front.data;
+      front = front.next;
+      size--;
+      if (isEmpty()) {
+          rear = null;
+      }
+      return item;
+  }
+
+  // Return the element at the front without removing it
+  public T peek() {
+      if (isEmpty()) {
+          throw new IllegalStateException("Queue is empty");
+      }
+      return front.data;
+  }
+
+  // Check if the queue is empty
+  public boolean isEmpty() {
+      return size == 0;
+  }
+
+  // Return the number of elements in the queue
+  public int size() {
+      return size;
+  }
+
+  // Clear the queue
+  public void clear() {
+      front = null;
+      rear = null;
+      size = 0;
+  }
+
+  // Print the elements of the queue
+  public void print() {
+      Node<T> current = front;
+      while (current != null) {
+          System.out.print(current.data + " ");
+          current = current.next;
+      }
+      System.out.println();
+  }
 }
